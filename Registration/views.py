6adc3899df86a42d4f8a63262f99213a3bd6ef38
Nguyen Thanh Vinh <vinhpro406@gmail.com>
2023-04-15 09:58:08 +0700

    add register center info in register page

--- Registration/views.py
+++ Registration/views.py
@@ -4,33 +4,26 @@ from django.shortcuts import render
 
 from .models import RegisterData, Owners, CarSpecs
 
-from django.views.generic import ListView
-
-
-class ContactListView(ListView):
-    paginate_by = 2
-    model = RegisterData
-
 
 # Create your views here.
 @login_required(login_url='login')
 def display(request):
     cars = RegisterData.objects.all()
-    paginator = Paginator(cars, 30)  # Show 25 contacts per page.
+    paginator = Paginator(cars, 30)  # Show 30 contacts per page.
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {'page_obj': page_obj}
     return render(request, 'display.html', context)
 
 
-@login_required()
+@login_required(login_url='login')
 def owner(request):
     owner_data = Owners.objects.all()
     context = {'owners': owner_data}
     return render(request, 'owner.html', context)
 
 
-@login_required()
+@login_required(login_url='login')
 def car_models(request):
     car_model = CarSpecs.objects.all()
     context = {'cars': car_model}
--- UserManagement/views.py
+++ UserManagement/views.py
@@ -1,8 +1,10 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import permission_required, login_required
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import permission_required
+
+from .forms import RegisterForm
 
 
 # Create your views here.
@@ -39,15 +41,16 @@ def logout_request(request):
     return redirect('login')
 
 
+@login_required(login_url='login')
 @permission_required('UserManagement.add_user', raise_exception=True)
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, f'Your account has been created. You can log in now!')
-            return redirect('login')
+            return redirect('register')
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
     context = {'form': form}
     return render(request, 'register.html', context)
