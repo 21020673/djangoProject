997ce98836d95e5d2ce8230dd0dc39f999dcb618
Nguyen Thanh Vinh <vinhpro406@gmail.com>
2023-04-28 15:16:22 +0700

    use class based view use database.html as template for all tables

--- Registration/urls.py
+++ Registration/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.display, name='database'),
+    path('', views.RegisterDataView.as_view(), name='database'),
     path('owner/<int:owner_id>/', views.owner, name='owner'),
-    path('owner/', views.owner, kwargs={'owner_id': None}, name='owner'),
-    path('car/<int:car_id>/', views.car_detail, name='car_detail'),
-    path('cars/', views.car_models, name='cars'),
+    path('owner/', views.OwnerView.as_view(), name='owner'),
+    path('car/<pk>/', views.CarDetail.as_view(), name='car_detail'),
+    path('cars/', views.CarsView.as_view(), name='cars'),
 ]
--- Registration/views.py
+++ Registration/views.py
@@ -1,47 +1,78 @@
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.generic import DetailView, ListView
 
 from .models import RegisterData, Owners, CarSpecs
 
 
 # Create your views here.
-@login_required(login_url='login')
-def display(request):
-    if request.GET.get('search'):
-        search_item = request.GET.get('search')
-        cars = RegisterData.objects.filter(Q(owner__name__icontains=search_item) |
-                                           Q(license_plate__license_plate__icontains=search_item) |
-                                           Q(license_plate__model__make__icontains=search_item) |
-                                           Q(license_plate__model__model__icontains=search_item)
-        )
-    else:
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class RegisterDataView(ListView):
+    paginate_by = 20
+    model = RegisterData
+    template_name = 'register_data.html'
+
+    def get_queryset(self):
         cars = RegisterData.objects.all()
-    paginator = Paginator(cars, 10)  # Show 10 contacts per page.
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {'page_obj': page_obj}
-    return render(request, 'display.html', context)
+        if self.request.GET.get('search'):
+            search_item = self.request.GET.get('search')
+            cars = RegisterData.objects.filter(Q(owner__name__icontains=search_item) |
+                                               Q(license_plate__license_plate__icontains=search_item) |
+                                               Q(license_plate__model__make__icontains=search_item) |
+                                               Q(license_plate__model__model__icontains=search_item) |
+                                               Q(register_center__city_province__icontains=search_item)
+                                               )
+        return cars
+
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class OwnerView(ListView):
+    paginate_by = 20
+    model = Owners
+    template_name = 'owner.html'
+
+    def get_queryset(self):
+        owners = Owners.objects.all()
+        if self.request.GET.get('search'):
+            search_item = self.request.GET.get('search')
+            owners = Owners.objects.filter(Q(name__icontains=search_item) |
+                                           Q(type__icontains=search_item) |
+                                           Q(address__icontains=search_item) |
+                                           Q(phone__icontains=search_item) |
+                                           Q(city_province__icontains=search_item)
+                                           )
+        return owners
+
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class CarsView(ListView):
+    paginate_by = 20
+    model = CarSpecs
+    template_name = 'cars.html'
+
+    def get_queryset(self):
+        cars = CarSpecs.objects.all()
+        if self.request.GET.get('search'):
+            search_item = self.request.GET.get('search')
+            cars = CarSpecs.objects.filter(Q(make__icontains=search_item) |
+                                           Q(model__icontains=search_item) |
+                                           Q(year_from__icontains=search_item) |
+                                           Q(body_type__icontains=search_item)
+                                           )
+        return cars
 
 
 @login_required(login_url='login')
 def owner(request, owner_id):
-    owner_data = Owners.objects.all()
-    if owner_id is not None:
-        owner_data = owner_data.filter(owner_id=owner_id)
-    context = {'owners': owner_data}
+    owner_data = Owners.objects.filter(owner_id=owner_id)
+    context = {'page_obj': owner_data}
     return render(request, 'owner.html', context)
 
 
-@login_required(login_url='login')
-def car_models(request):
-    cars = CarSpecs.objects.all()
-    context = {'cars': cars}
-    return render(request, 'cars.html', context)
-
-
-def car_detail(request, car_id):
-    car_model = CarSpecs.objects.get(id=car_id)
-    context = {'car': car_model}
-    return render(request, 'car_detail.html', context)
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class CarDetail(DetailView):
+    model = CarSpecs
+    template_name = 'car_detail.html'
+    context_object_name = 'car'
