f7e2423668154d3a55f9c47d43460363bfa914ab
Nguyen Thanh Vinh <vinhpro406@gmail.com>
2023-04-28 21:36:09 +0700

    use django-crispy-form to render form

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'UserManagement',
-    'Registration'
+    'Registration',
+    'crispy_forms',
+    "crispy_tailwind",
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+
+CRISPY_TEMPLATE_PACK = "tailwind"
