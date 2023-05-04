4a5cac7044d103aba9840f9bc9f6491d75a57e13
Nguyen Thanh Vinh <vinhpro406@gmail.com>
2023-05-04 16:08:29 +0700

    merge

--- Registration/urls.py
+++ Registration/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('database/', views.RegisterDataView.as_view(), name='database'),
+    path('database/register_data/', views.RegisterDataView.as_view(), name='database'),
     path('database/owner/<int:owner_id>/', views.owner, name='owner'),
     path('database/owner/', views.OwnerView.as_view(), name='owner'),
     path('database/car/<pk>/', views.CarDetail.as_view(), name='car_detail'),
