7ad76cbbdb86091ffe20d7ce272ecfee9cbed3a7
BaoLam23 <baolammmm@gmail.com>
2023-06-11 01:11:37 +0700

    minor fixes

---
+++ staticfiles/js/filter_hide.js
@@ -0,0 +1,18 @@
+let filter1 = document.getElementById('filter-1');
+let filter2 = document.getElementById('filter-2');
+let filter3 = document.getElementById('filter-3');
+let filter4 = document.getElementById('filter-4');
+
+function hide() {
+    filter1.style.visibility = 'hidden';
+    filter2.style.visibility = 'hidden';
+    filter3.style.visibility = 'hidden';
+    filter4.style.visibility = 'hidden';
+}
+
+function unhide() {
+    filter1.style.visibility = 'visible';
+    filter2.style.visibility = 'visible';
+    filter3.style.visibility = 'visible';
+    filter4.style.visibility = 'visible';
+}
